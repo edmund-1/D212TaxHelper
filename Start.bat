02fc542461c5b424e1283a170b93098332315caa
Edmund Spatariu <spatariu@gmail.com>
2026-04-07 19:17:16 +0300

    Start.bat: launch server minimized and exit immediately

--- Start.bat
+++ Start.bat
@@ -8,11 +8,11 @@ echo   ===================================
 echo.
 
 cd /d "%~dp0"
-start "" node server.js
+start /min "" node server.js
 timeout /t 2 /nobreak >nul
 start http://localhost:3000
 
 echo   Server running at http://localhost:3000
 echo   Use Stop.bat to stop the server.
 echo.
-timeout /t 10 /nobreak >nul
+exit
